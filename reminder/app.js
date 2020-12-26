e478c9fb46eb06a545a2abc9018ba5f375d2c32c
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-26 19:19:17 +0100

    WIP app.js

--- reminder/app.js
+++ reminder/app.js
@@ -11,3 +11,18 @@ const fn = (item) => console.log('Podany argument to ' + item)
 const fn = (item) => (
     console.log('podany argument to ' + item)
 )
+
+// ----------------------------------------------------------------------
+
+const fn = (item, item2) => {
+    return `Podany argument to: ${item} i ${item2}`
+}
+const result = fn('hej!', 'ho!')
+// result zawiera 'Podany argument to: hej! i ho!'
+
+//  to samo ponizej
+
+const fn = (item, item2) => (
+    `Podany argument to: ${item} i ${item2}`
+)
+const result = fn('Hej!', 'Ho!')
