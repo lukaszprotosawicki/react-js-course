d88b46c5b771374e30b54b9e748e64519d3aaa36
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-26 19:56:09 +0100

    WIP add metod map()2 app.js

--- reminder/app.js
+++ reminder/app.js
@@ -72,7 +72,15 @@ console.log(allUsers);
 
 const users = ['adam', 'bogdan', 'czarek', 'darek'];
 
-const usersFirstLetterUpperCase = users.map(user =>
-    user[0].toUpperCase())
+const usersFirstLetterUpperCase = users.map(user => user[0].toUpperCase())
 console.log(usersFirstLetterUpperCase);
-    // ['A', 'B', 'C', 'D']
+// ['A', 'B', 'C', 'D']
+
+// map() przykład 2
+//metoda map zwraca nową tablicę o tej samej długości
+
+const numbers = [2, 3, 4]
+const doubleNumber = numbers.map(number => number * 2)
+console.log(doubleNumber);
+// [4, 6, 8]
+
