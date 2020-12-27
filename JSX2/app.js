51d2a24b951b5b9b335a2235dbe599054524c0d9
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-27 18:53:43 +0100

    add app.js

---
+++ JSX2/app.js
@@ -0,0 +1,14 @@
+const header = <h1 className="title" >Witaj na stronie!</h1>
+const main = (
+    <div>
+        <h1>Pierwszy artykuł</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat error odio saepe et asperiores explicabo architecto quisquam molestiae exercitationem,
+        eaque vitae rem! Officiis minima, eaque temporibus nihil quae aliquid facilis?Aliquam deserunt sapiente perferendis molestiae eligendi facilis nihil obcaecati temporibus,
+        quo quas ipsam dolorum incidunt assumenda maxime soluta placeat natus expedita aut reiciendis nesciunt, numquam tempore consectetur cumque vero. Minima?</p>
+    </div>
+)
+const footer = (
+    <footer>
+        <p>Stopka</p>
+    </footer>
+)
