6d8648f776a0340e5218cb5132aa64b4850bef05
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-27 15:05:28 +0100

    klasa dodawanie metod do prototypow i instancji

--- reminder/obiekty-klasy-instancje-dziedziczenie.js
+++ reminder/obiekty-klasy-instancje-dziedziczenie.js
@@ -8,3 +8,39 @@ const Barcelona = new City();
 
 // powstają dwa różne, niepołączone obiekty będące instancją City.
 // City{}
+
+// klasa = właściwości instancji 
+
+class Country {
+    constructor(name, capital, population) {
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+    }
+}
+
+const poland = new Country('Polska', 'Warszawa', '39000000')
+console.log(poland)
+// Country { name: 'Polska', capital: 'Warszawa', population: '39000000' }
+
+// klasa - dodawanie metod do prototypów i instancji
+
+class Country {
+    constructor(name) {
+        this.name = name; // właściwość każdej instancji
+        this.showName = () => console.log(this.name) // metoda każdej instancji
+    }
+    // Wszystkie metody tworzone w klasie znajdują się w prototypie klasy, do której dostęp mają wszystkie instancje.
+    showCountryName() {
+        console.log(`Nazwa kraju to ${this.name}`);
+    }
+}
+
+const Poland = new Country('Polska');
+const Spain = new Country('Hiszpania');
+
+Poland.showCountryName(); // Nazwa kraju to Polska
+Spain.showCountryName(); // Nazwa kraju to Hiszpania
+Poland.showName(); // Polska
+Spain.showName(); // Hiszpania
+
