16f18c71a535143c08c662a3f28c508e5be8cdce
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-27 18:46:37 +0100

    WIP JSX

--- JSX/app.js
+++ JSX/app.js
@@ -2,3 +2,24 @@ console.log(React);
 console.log(ReactDOM);
 
 const element = <div>Pierwszy element React</div>
+
+const element2 = React.createElement(
+    "div",
+    null,
+    "Pierwszy element React"
+)
+
+
+const element3 = (
+    <div>
+        <p>Tekst</p>
+    </div>
+
+)
+
+const element4 = (
+    <div>
+        <section></section>
+        <section></section>
+    </div>
+)
