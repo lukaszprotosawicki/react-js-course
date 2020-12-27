a1540037a9896237b219ba79dd56e0b5e991d2f5
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-27 14:48:09 +0100

    add class

---
+++ reminder/obiekty-klasy-instancje-dziedziczenie.js
@@ -0,0 +1,10 @@
+// deklaracja klasy
+class City {
+}
+
+// tworzenie instancji klasy
+const Poznan = new City();
+const Barcelona = new City();
+
+// powstają dwa różne, niepołączone obiekty będące instancją City.
+// City{}
