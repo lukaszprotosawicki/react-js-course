4e6a60746312b73ef1b423b72aad0ea2f7afb908
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-28 16:17:05 +0100

    WIP app.js

--- Add-sign/app.js
+++ Add-sign/app.js
@@ -3,23 +3,28 @@
 class App extends React.Component {
 
     state = {
-        text: ""
+        text: "",
+        number: 2
     }
 
-    handleClick = () => {
+    handleClick() {
         // this.state.text += "a"
         // console.log(this.state.text)
         const letter = "a";
 
-        this.setState({
+        // this.setState({
+        //     text: this.state.text + letter
+        // })
+        this.setState(() => ({
             text: this.state.text + letter
         })
+        )
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.handleClick}>Dodaj "A"</button>
+                <button onClick={this.handleClick.bind(this)}>Dodaj "A"</button>
                 <h1>{this.state.text}</h1>
             </div>
         )
