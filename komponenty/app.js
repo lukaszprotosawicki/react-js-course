1a7814973b9962cbe2d67ac2541c76680b5821fa
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-28 14:36:28 +0100

    add App to app.js

--- komponenty/app.js
+++ komponenty/app.js
@@ -1,4 +1,14 @@
 // komponent funkcyjny, bezstanowy
 
+const App = () => {
+    return (
+        <div>
+            <h1>Pierwszy komponent</h1>
+        </div>
+    )
+}
 
 // komponent klasowy, stanowy
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
