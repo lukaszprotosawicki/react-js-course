6ca93976e3d63ba5a668aad31d1e871b65a1b514
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-28 16:51:37 +0100

    cleared code app.js

--- Add-sign/app.js
+++ Add-sign/app.js
@@ -1,26 +1,13 @@
 // Przycisk = po kliknieciu dodawana jest litera do tekstu
 
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         text: ""
-    //     }
-    //     this.handleClick = this.handleClick.bind(this)
-    // }
     state = {
         text: "",
         number: 2
     }
 
     handleClick = () => {
-        // this.state.text += "a"
-        // console.log(this.state.text)
         const letter = "a";
-
-        // this.setState({
-        //     text: this.state.text + letter
-        // })
         this.setState(() => ({
             text: this.state.text + letter
         })
