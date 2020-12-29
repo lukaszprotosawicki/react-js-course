eaf2e0af903d162d0327352e32903901d6ad8bd9
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-29 16:43:19 +0100

    cleared code  app.js

--- text-imput/app.js
+++ text-imput/app.js
@@ -3,14 +3,14 @@ class App extends React.Component {
         value: ""
     }
 
-    handleImputChange = (e) => {
+    handleChange = (e) => {
         console.log(e.target.value);
         this.setState({
             value: e.target.value
         })
     }
 
-    handleResetClick = () => {
+    handleClick = () => {
         this.setState({
             value: ""
         })
@@ -19,8 +19,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <input value={this.state.value} placeholder="wpisz..." onChange={this.handleImputChange} type="text" />
-                <button onClick={this.handleResetClick}>Reset</button>
+                <input value={this.state.value} placeholder="wpisz..." onChange={this.handleChange} type="text" />
+                <button onClick={this.handleClick}>Reset</button>
                 <h1 className="title">{this.state.value.toUpperCase()}</h1>
             </div>
         )
