087d25d1b5226ebbdf1555017ce4df34d35ff7d9
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-29 17:20:50 +0100

    add constructor and hadneclick

--- button-show-hide/app.js
+++ button-show-hide/app.js
@@ -1,5 +1,31 @@
-class Message extends ReadableStreamDefaultController.Component {
+class Message extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            messageIsActive: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+
+        this.setState({
+            messageIsActive: !this.state.messageIsActive
+        })
+    }
+
+    render() {
+        console.log(this.state)
+        const text = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione voluptatem in magnam amet dignissimos veritatis numquam, aliquam asperiores ex tenetur eum, voluptatum aliquid accusantium molestiae nesciunt error commodi blanditiis iste?"
+
+        return (
+            <div>
+                <button onClick={this.handleClick}>Pokaż</button>
+                <p>{text}</p>
+            </div>
+        )
+    }
 }
 
-ReactDom.render(<Message />, document.getElementById("root"))
+ReactDOM.render(<Message />, document.getElementById("root"))
