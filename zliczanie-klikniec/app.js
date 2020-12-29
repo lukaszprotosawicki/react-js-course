f89f6436621dcc7e7e094dedda860f09bf403467
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-29 20:26:56 +0100

    add handleMathClick action in button

--- zliczanie-klikniec/app.js
+++ zliczanie-klikniec/app.js
@@ -1,12 +1,28 @@
 class Counter extends React.Component {
+
+    state = {
+        count: 0,
+        result: 0
+    }
+
+    handleMathClick(type, number) {
+        if (type === "substraction") {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result - number
+            }))
+        }
+    }
+
     render() {
         return (
             <div>
-                <button></button>
-                <button></button>
-                <button></button>
-                <h1></h1>
-                <h1></h1>
+                <button onClick={this.handleMathClick}>-10</button>
+                <button>-1</button>
+                <button>Reset</button>
+                <button>1</button>
+                <h1>Liczba kliknięć: {this.state.count}</h1>
+                <h1>Wynik: {this.state.result}</h1>
             </div>
         )
     }
