42f0a89dc26e6e384e89e426375bb974a2528196
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-29 20:55:47 +0100

    add function addition +1 and +10

--- zliczanie-klikniec/app.js
+++ zliczanie-klikniec/app.js
@@ -7,15 +7,26 @@ class Counter extends React.Component {
 
     handleMathClick(type, number = 1) {
         if (type === "substraction") {
-            this.setState(prevState => ({
-                count: prevState.count + 1,
-                result: prevState.result - number
-            }))
+            this.setState(prevState => (
+                {
+                    count: prevState.count + 1,
+                    result: prevState.result - number
+                }
+            ))
         } else if (type === "reset") {
-            this.setState(prevState => ({
-                count: prevState.count + 1,
-                result: 0
-            }))
+            this.setState(prevState => (
+                {
+                    count: prevState.count + 1,
+                    result: 0
+                }
+            ))
+        } else if (type === "addition") {
+            this.setState(prevState => (
+                {
+                    count: prevState.count + 1,
+                    result: prevState.result + number
+                }
+            ))
         }
     }
 
@@ -25,7 +36,8 @@ class Counter extends React.Component {
                 <button onClick={this.handleMathClick.bind(this, "substraction", 10)}>-10</button>
                 <button onClick={() => this.handleMathClick("substraction", 1)}>-1</button>
                 <button onClick={this.handleMathClick.bind(this, "reset")}>Reset</button>
-                <button>1</button>
+                <button onClick={this.handleMathClick.bind(this, "addition", 1)}>+1</button>
+                <button onClick={this.handleMathClick.bind(this, "addition", 10)}>+10</button>
                 <h1>Liczba kliknięć: {this.state.count}</h1>
                 <h1>Wynik: {this.state.result}</h1>
             </div>
