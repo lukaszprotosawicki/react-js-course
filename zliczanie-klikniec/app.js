3931f0efdd61994dcd8f7953f3b461ab53cfecb5
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-29 20:18:14 +0100

    add render

--- zliczanie-klikniec/app.js
+++ zliczanie-klikniec/app.js
@@ -1,5 +1,16 @@
 class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <button></button>
+                <button></button>
+                <button></button>
+                <h1></h1>
+                <h1></h1>
+            </div>
+        )
+    }
 
 }
 
-React.DOM.render(<Counter />, document.getElementById('root'))
+ReactDOM.render(<Counter />, document.getElementById('root'))
