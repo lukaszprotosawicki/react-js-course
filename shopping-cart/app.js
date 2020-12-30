f99ebca20b3aacf74cef6b28453c7cb9ed729b46
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-30 17:19:12 +0100

    add function handleAddCart

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,7 +1,7 @@
 class App extends React.Component {
     state = {
         availableProducts: 10,
-        shoppingCart: 2,
+        shoppingCart: 0,
     }
 
     handleRemoveFromCart = () => {
@@ -10,12 +10,18 @@ class App extends React.Component {
         })
     }
 
+    handleAddToCart = () => {
+        this.setState({
+            shoppingCart: this.state.shoppingCart + 1,
+        })
+    }
+
     render() {
         return (
             <div>
                 <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
                 <span> {this.state.shoppingCart} </span>
-                <button>+</button>
+                <button onClick={this.handleAddToCart}>+</button>
             </div>
         )
     }
