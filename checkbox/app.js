5b6adbd93cdfe55b5c52f4bb36770206744b9300
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-30 14:52:10 +0100

    add displayMessage

--- checkbox/app.js
+++ checkbox/app.js
@@ -12,12 +12,20 @@ class CheckboxAgeConfirmation extends React.Component {
             isConfirmed: !this.state.isConfirmed
         })
     }
+    displayMessage = () => {
+        if (this.state.isConfirmed) {
+            return <PositiveMessage />
+        } else {
+            return <NegativeMessage />
+        }
+    }
     render() {
         return (
             <div>
                 <h1>Kup bilet na horror roku!</h1>
                 <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed} />
                 <label htmlFor="age">Mam co najmniej 16 lat</label>
+                {this.displayMessage()}
             </div>
         )
     }
