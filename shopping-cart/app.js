1310c588d64d3caa16ea1cae43630752fbd04483
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-30 17:05:55 +0100

    add html + css

---
+++ shopping-cart/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    state = {
+
+    }
+    render() {
+        return (
+            <div>{ }</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
