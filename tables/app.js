111a7ba4ef56237a24dbe90ffd6d81d869d233f2
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-30 21:05:39 +0100

    wip app.js

--- tables/app.js
+++ tables/app.js
@@ -1,5 +1,14 @@
 class ListItem extends React.Component {
-
+    state = {
+        items: ["jablko", "sliwka", "gruszka"]
+    }
+    render() {
+        return (
+            <ul>
+                {this.state.items.map(item => <li key={item}> {`owoc ${item}`}</li>)}
+            </ul>
+        )
+    }
 }
 
 ReactDOM.render(<ListItem />, document.getElementById('root'))
