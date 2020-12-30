1689bcbf04dd455d25f63dcd5ff90ab7eedef733
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-30 20:43:58 +0100

    cleared code degradecion

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -24,12 +24,13 @@ class App extends React.Component {
     }
 
     render() {
+        const { shoppingCart, availableProducts } = this.state
         return (
             <div>
-                <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-                <span style={this.state.shoppingCart === 0 ? { opacity: 0.3 } : {}}> {this.state.shoppingCart} </span>
-                <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
-                {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
+                <button disabled={shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
+                <span style={shoppingCart === 0 ? { opacity: 0.3 } : {}}> {shoppingCart} </span>
+                <button disabled={shoppingCart === availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
+                {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
             </div>
         )
     }
