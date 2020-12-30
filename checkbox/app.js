549ac1194a885a3902771a019861d9052392ea44
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-30 14:48:27 +0100

    add state and function handleCheckboxChange

--- checkbox/app.js
+++ checkbox/app.js
@@ -1,10 +1,23 @@
+const PositiveMessage = () => <p>Możesz obejrzeć film. Zapraszamy!</p>
+const NegativeMessage = () => <p>Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!</p>
+
+
 class CheckboxAgeConfirmation extends React.Component {
+
+    state = {
+        isConfirmed: false,
+    }
+    handleCheckboxChange = () => {
+        this.setState({
+            isConfirmed: !this.state.isConfirmed
+        })
+    }
     render() {
         return (
             <div>
                 <h1>Kup bilet na horror roku!</h1>
-                <input type="checkbox" id="age" />
-                <label htmlFor="age"></label>
+                <input type="checkbox" id="age" onChange={this.handleCheckboxChange} checked={this.state.isConfirmed} />
+                <label htmlFor="age">Mam co najmniej 16 lat</label>
             </div>
         )
     }
