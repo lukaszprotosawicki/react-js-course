bb8d3cb599da08a0d0a553df9c4428b3d9d81175
lukaszprotosawicki <lukas.es.p@o2.pl>
2020-12-31 15:14:10 +0100

    add data to app.js

--- tables/app.js
+++ tables/app.js
@@ -1,10 +1,33 @@
+const data = {
+    users: [
+        {
+            id: 1,
+            age: 32,
+            name: "Lukasz",
+        },
+        {
+            id: 2,
+            age: 29,
+            name: "Patryk",
+        },
+        {
+            id: 3,
+            age: 33,
+            name: "Kamil",
+        }
+
+    ]
+}
+
+
+
 const Item = (props) => <li>{`owoc ${props.content}`}</li>
 
 
 class ListItem extends React.Component {
-    state = {
-        items: ["jablko", "sliwka", "gruszka"]
-    }
+    // state = {
+    //     items: ["jablko", "sliwka", "gruszka"]
+    // }
     render() {
         const Items = this.state.items.map(item => <Item key={item} content={item} />)
         return (
