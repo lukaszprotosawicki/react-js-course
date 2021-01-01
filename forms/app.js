2f5a0c8e1469e8e4c36ab3b045420c6ed5a230aa
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-01 22:01:29 +0100

    optimization

--- forms/app.js
+++ forms/app.js
@@ -6,27 +6,40 @@ class Form extends React.Component {
         number: "2"
     }
 
-    handleCityChange = event => {
-        this.setState({
-            city: event.target.value
-        })
-    }
+    // handleCityChange = event => {
+    //     this.setState({
+    //         city: event.target.value
+    //     })
+    // }
 
-    handleTextChange = e => {
-        this.setState({
-            text: e.target.value
-        })
-    }
+    // handleTextChange = e => {
+    //     this.setState({
+    //         text: e.target.value
+    //     })
+    // }
 
-    handleIsLovedChange = e => {
-        this.setState({
-            isLoved: e.target.checked
-        })
-    }
-    handleVisitsChange(e) {
-        this.setState({
-            number: e.target.value
-        })
+    // handleIsLovedChange = e => {
+    //     this.setState({
+    //         isLoved: e.target.checked
+    //     })
+    // }
+    // handleVisitsChange(e) {
+    //     this.setState({
+    //         number: e.target.value
+    //     })
+    // }
+
+
+    handleChange = e => {
+        if (e.target.type === "checkbox") {
+            this.setState({
+                [e.target.name]: e.target.checked
+            })
+        } else {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        }
     }
 
     render() {
@@ -34,21 +47,21 @@ class Form extends React.Component {
             <div>
                 <label>
                     Podaj miasto:
-                    <input value={this.state.city} onChange={this.handleCityChange} type="text" />
+                    <input name="city" value={this.state.city} onChange={this.handleChange} type="text" />
                 </label>
                 <br />
                 <label>Napisz coś o tym mieście
-                    <textarea value={this.state.text} onChange={this.handleTextChange}></textarea>
+                    <textarea name="text" value={this.state.text} onChange={this.handleChange}></textarea>
                 </label>
                 <br />
                 <label>
                     Czy lubisz to miasto?
-                    <input type="checkbox" checked={this.state.isLoved} onChange={this.handleIsLovedChange} />
+                    <input name="isLoved" type="checkbox" checked={this.state.isLoved} onChange={this.handleChange} />
                 </label>
                 <br />
                 <label>
                     Ile razy byliście w tym mieście?
-                    <select value={this.state.number} onChange={this.handleVisitsChange.bind(this)}>
+                    <select name="number" value={this.state.number} onChange={this.handleChange}>
                         <option value="0">0</option>
                         <option value="1">1</option>
                         <option value="2">2</option>
