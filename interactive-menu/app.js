a8f7064fc0405b4def3b81b53698f0f54992e10d
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-01 19:13:21 +0100

    add update  header.js

--- interactive-menu/app.js
+++ interactive-menu/app.js
@@ -1,12 +1,12 @@
 class App extends React.Component {
     state = {
         items: [
-            { id: 1, name: "kawa", active: true },
+            { id: 1, name: "kawa", active: false },
             { id: 2, name: "ziemniaki", active: false },
             { id: 3, name: "bułka", active: false },
             { id: 4, name: "banan", active: false },
             { id: 5, name: "herbata", active: false },
-            { id: 6, name: "makaron", active: true },
+            { id: 6, name: "makaron", active: false },
         ]
     }
     render() {
--- interactive-menu/header.js
+++ interactive-menu/header.js
@@ -1,11 +1,12 @@
 const Header = (props) => {
 
     const activeItems = props.items.filter(item => item.active)
+    const number = activeItems.length
 
     return (
         <header>
             <h2>Wielkość zamówienia: {activeItems.length}</h2>
-            <h2>Do zapłaty: {activeItems.length * 10} złotych</h2>
+            <h2>Do zapłaty: {number ? `${number * 10} złotych` : 'nie kupujesz, to nie płacisz'} </h2>
         </header>
     )
 }
