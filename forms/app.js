dc36ddf6b26a25728248e486be198b48f89243d0
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-01 21:46:19 +0100

    WIP app.js mixed

--- forms/app.js
+++ forms/app.js
@@ -1,20 +1,46 @@
 class Form extends React.Component {
-    state = {}
+    state = {
+        city: "Gorzów",
+        text: "",
+        isLoved: true
+    }
+
+    handleCityChange = event => {
+        this.setState({
+            city: event.target.value
+        })
+    }
+
+    handleTextChange = e => {
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    handleIsLovedChange = e => {
+        this.setState({
+            isLoved: e.target.checked
+        })
+    }
+
     render() {
         return (
-            <form>
+            <div>
                 <label>
-                    Podaj imię:
-                    <input type="text" name="name" />
+                    Podaj miasto:
+                    <input value={this.state.city} onChange={this.handleCityChange} type="text" />
+                </label>
+                <br />
+                <label>Napisz coś o tym mieście
+                    <textarea value={this.state.text} onChange={this.handleTextChange}></textarea>
                 </label>
                 <br />
                 <label>
-                    Podaj email:
-                    <input type="email" name="email" />
+                    Czy lubisz to miasto?
+                    <input type="checkbox" checked={this.state.isLoved} onChange={this.handleIsLovedChange} />
                 </label>
-                <br />
-                <button>Zapisz</button>
-            </form>
+
+            </div>
         );
     }
 }
