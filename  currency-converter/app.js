fc6ea160e51d016e49638513f4251dcbd4398651
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-01 22:04:17 +0100

    initial commit currency-counter

---
+++  currency-converter/app.js
@@ -0,0 +1,9 @@
+class ExchangeCounter extends React.Component {
+    render() {
+        return (
+            <div>Kantor</div>
+        )
+    }
+}
+
+ReactDOM.render(<ExchangeCounter />, document.getElementById('root'))
