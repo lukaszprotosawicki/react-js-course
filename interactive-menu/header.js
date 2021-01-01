4ec8b987752408bc5d1f13588fcdcf7a257fe347
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-01 19:09:36 +0100

    add activeItems filter to header.js

--- interactive-menu/header.js
+++ interactive-menu/header.js
@@ -1,5 +1,11 @@
-const Header = () => {
+const Header = (props) => {
+
+    const activeItems = props.items.filter(item => item.active)
+
     return (
-        <header>Podsumowanie zamówienia</header>
+        <header>
+            <h2>Wielkość zamówienia: {activeItems.length}</h2>
+            <h2>Do zapłaty: {activeItems.length * 10} złotych</h2>
+        </header>
     )
 }
--- interactive-menu/listitems.js
+++ interactive-menu/listitems.js
@@ -4,6 +4,9 @@ const ListItems = (props) => {
             <li>1</li>
             <li>2</li>
             <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
         </ul>
     )
 }
