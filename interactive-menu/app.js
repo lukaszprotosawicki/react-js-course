ef7e8b9c212d482478f535fe50aae6f231162798
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-01 18:51:21 +0100

    add state items to app.js

--- interactive-menu/app.js
+++ interactive-menu/app.js
@@ -1,5 +1,14 @@
 class App extends React.Component {
-    state = {}
+    state = {
+        items: [
+            { id: 1, content: "kawa", active: true },
+            { id: 2, content: "ziemniaki", active: false },
+            { id: 3, content: "bułka", active: false },
+            { id: 4, content: "banan", active: false },
+            { id: 5, content: "herbata", active: false },
+            { id: 6, content: "makaron", active: true },
+        ]
+    }
     render() {
         return (
             <div>Menu</div>
