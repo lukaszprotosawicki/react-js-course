db71094267529619c180b1e812c8f57c60670062
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-02 16:58:15 +0100

    finish currency-counter

---  currency-converter/app.js
+++  currency-converter/app.js
@@ -1,35 +1,49 @@
 const Cash = (props) => (
-    <div>{props.title} {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
+    <div>{props.title} {props.cash <= 0 ? "" : (props.cash / props.ratio * props.price).toFixed(2)}</div>
 )
 
 class ExchangeCounter extends React.Component {
     state = {
         amount: "",
-        product: 'gas'
-        // ratioDollar: 3.6,
-        // ratioEuro: 4.2,
+        product: "gas"
+
     }
 
-    currencies = [
-        {
-            id: 1,
-            name: 'dollar',
-            ratio: 3.6,
-            title: "Wartość w dolarach"
-        },
-        {
-            id: 2,
-            name: 'dollar',
-            ratio: 4.1,
-            title: "Wartość w euro"
-        },
-        {
-            id: 3,
-            name: 'pound',
-            ratio: 4.9,
-            title: "Wartość w funtach"
-        },
-    ]
+    static defaultProps = {
+        currencies: [
+            {
+                id: 0,
+                name: 'zloty',
+                ratio: 1,
+                title: "Wartość w złotówkach"
+            },
+            {
+                id: 1,
+                name: 'dollar',
+                ratio: 3.6,
+                title: "Wartość w dolarach"
+            },
+            {
+                id: 2,
+                name: 'euro',
+                ratio: 4.1,
+                title: "Wartość w euro"
+            },
+            {
+                id: 3,
+                name: 'pound',
+                ratio: 4.9,
+                title: "Wartość w funtach"
+            },
+        ],
+        prices: {
+            electricity: 0.53,
+            gas: 5.32,
+            oranges: 3.45,
+        }
+    }
+
+
 
     handleChange = e => {
         this.setState({
@@ -38,19 +52,27 @@ class ExchangeCounter extends React.Component {
     }
     handleSelect = e => {
         this.setState({
-            product: e.target.value
+            product: e.target.value,
+            amount: "",
         })
+    }
+
+    selectPrice(select) {
+        return this.props.prices[select]
     }
 
     insertSuffix(select) {
         if (select === "electricity") return <em> Kwh</em>
         else if (select === "gas") return <em> Litrów</em>
         else if (select === "oranges") return <em> Kilogramów</em>
+        else return null
     }
     render() {
         const { amount, product } = this.state;
-        const calculators = this.currencies.map(currency => (
-            <Cash key={currency.id} ratio={currency.ratio} title={currency.title} cash={amount} />
+        const price = this.selectPrice(product);
+
+        const calculators = this.props.currencies.map(currency => (
+            <Cash key={currency.id} ratio={currency.ratio} title={currency.title} cash={amount} price={price} />
         ))
         return (
             <div className="app">
