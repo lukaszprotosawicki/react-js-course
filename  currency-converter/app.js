b2571ec694a1f00a76fa5129a62df0a0636bf26b
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-02 16:00:54 +0100

    add table to app.js

---  currency-converter/app.js
+++  currency-converter/app.js
@@ -1,22 +1,34 @@
-// const Dollars = (props) => (
-//     <div>Wartość w dolarach : {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
-// )
-
-// const Euros = (props) => (
-//     <div>Wartość w euro : {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
-// )
-
 const Cash = (props) => (
     <div>{props.title} {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
 )
 
-
 class ExchangeCounter extends React.Component {
     state = {
         amount: "",
-        ratioDollar: 3.6,
-        ratioEuro: 4.2,
+        // ratioDollar: 3.6,
+        // ratioEuro: 4.2,
     }
+
+    currencies = [
+        {
+            id: 1,
+            name: 'dollar',
+            ratio: 3.6,
+            title: "Wartość w dolarach"
+        },
+        {
+            id: 2,
+            name: 'dollar',
+            ratio: 4.1,
+            title: "Wartość w euro"
+        },
+        {
+            id: 3,
+            name: 'pound',
+            ratio: 4.9,
+            title: "Wartość w funtach"
+        },
+    ]
 
     handleChange = e => {
         this.setState({
@@ -32,10 +44,8 @@ class ExchangeCounter extends React.Component {
                         value={this.state.amount}
                         onChange={this.handleChange} />
                 </label>
-                {/* <Dollars cash={amount} ratio={ratioDollar} />
-                <Euros cash={amount} ratio={ratioEuro} /> */}
-                <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach: " />
-                <Cash cash={amount} ratio={ratioEuro} title="Wartość w euro: " />
+                {/* <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach: " />
+                <Cash cash={amount} ratio={ratioEuro} title="Wartość w euro: " /> */}
             </div>
         )
     }
