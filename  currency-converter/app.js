c632c60113349ffc8a6b406d4e885ef97c8bdb25
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-02 15:42:43 +0100

    WIP app.js

---  currency-converter/app.js
+++  currency-converter/app.js
@@ -1,7 +1,33 @@
+const Dollars = () => (
+    <div>Wartość w dolarach :</div>
+)
+
+const Euros = () => (
+    <div>Wartość w euro :</div>
+)
+
+
 class ExchangeCounter extends React.Component {
+    state = {
+        amount: ""
+    }
+
+    handleChange = e => {
+        this.setState({
+            amount: e.target.value
+        })
+    }
     render() {
         return (
-            <div>Kantor</div>
+            <div className="app">
+                <label>
+                    <input type="number"
+                        value={this.state.amount}
+                        onChange={this.handleChange} />
+                </label>
+                <Dollars />
+                <Euros />
+            </div>
         )
     }
 }
