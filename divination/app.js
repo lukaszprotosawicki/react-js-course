8c2386d05dd9b6a9f34cd193d37975d6e13ffeb7
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-02 19:27:20 +0100

    wip app.js

--- divination/app.js
+++ divination/app.js
@@ -1,12 +1,23 @@
-class App extends Component {
+class Draw extends React.Component {
     state = {
+        options: ["1", "2", "3"],
+        option: null
+    }
 
+    handleShowOption = () => {
+        const index = Math.floor(Math.random() * this.state.options.length)
+        this.setState({
+            option: this.state.options[index]
+        })
     }
     render() {
-        return ( 
-
-         );
+        return (
+            <div>
+                <button onClick={this.handleShowOption}>Zobacz wróżbę</button>
+                {this.state.option ? <h1>{this.state.option}</h1> : null}
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Draw />, document.getElementById('root'))
