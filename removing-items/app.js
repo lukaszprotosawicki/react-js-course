d4e98ae87a14412e284d6ba7cca5af7e4d939470
lukaszprotosawicki <lukas.es.p@o2.pl>
2021-01-02 19:49:06 +0100

    WIP app.js removing-items

--- removing-items/app.js
+++ removing-items/app.js
@@ -1,7 +1,31 @@
+
+const Person = (props) => {
+    return (
+        <li>
+            <span>{props.name}</span>
+            <button>Usuń</button>
+        </li>
+    )
+}
+
+
 class RemovingItems extends React.Component {
-    state = {}
+    state = {
+        people: [
+            { id: 1, name: 'Lukasz R' },
+            { id: 2, name: 'Bartek L' },
+            { id: 3, name: 'Piotr B' },
+            { id: 4, name: 'Kamil W' },
+            { id: 5, name: 'Angelika S' },
+            { id: 6, name: 'Renata P' }
+        ]
+    }
     render() {
-        return (  );
+        return (
+            <ul>
+                {this.state.people.map(person => <Person key={person.id} name={person.name} />)}
+            </ul>
+        );
     }
 }
 
