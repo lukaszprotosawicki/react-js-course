ebc2bbb78015d902184d8e8de36d0d4ed8f37e5a
lukasz protosawicki <lukas.es.p@o2.pl>
2021-01-19 15:21:17 +0100

    setState

--- setstate/app.js
+++ setstate/app.js
@@ -2,6 +2,7 @@ class Clicker extends React.Component {
     state = {
         number1: 0,
         number2: 0,
+        number3: 0,
     }
 
     handleClick1 = () => {
@@ -10,18 +11,28 @@ class Clicker extends React.Component {
         })
     }
 
-    handleClick3 = () => {
+    handleClick2 = () => {
         this.setState({
             number2: this.state.number2 + 3
         })
     }
+
+    handleClick3 = () => {
+            this.setState(() => ({
+            number3: this.state.number3 +1,
+            }))
+    }
+
+
     render() {
         return (
             <div>
                 <button onClick={this.handleClick1}>Podbij o 1</button>
                 <h1>{this.state.number1}</h1>
-                <button onClick={this.handleClick3}>Podbij o 3</button>
+                <button onClick={this.handleClick2}>Podbij o 3</button>
                 <h1>{this.state.number2}</h1>
+                <button onClick={this.handleClick3}>Podbij o 3</button>
+                <h1>{this.state.number3}</h1>
             </div>
         )
     }
